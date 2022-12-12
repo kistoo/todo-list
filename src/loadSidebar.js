b30222407b415a91a4b87ba47401abdd7557f7fa
kistoo <tomio.nakamura15@gmail.com>
2022-12-12 11:51:05 -0500

    Add event listener to display categories

--- src/loadSidebar.js
+++ src/loadSidebar.js
@@ -1,16 +1,21 @@
 import { dataStorage } from "./dataStorage";
+import { loadContent } from "./loadContent";
 
 function loadSidebar() {
     const sidebar = document.getElementById('sidebar');
 
-    const categories = dataStorage.categories.getCategory();
-    //this function need to be in dataStorage module
-    categories.forEach((category)=> {
+    //gets array with categories
+    const categories = dataStorage.categories.getCategories();
+    categories.forEach((category) => {
         const newCategory = document.createElement('div');
         newCategory.className = "category";
         newCategory.textContent = category.name;
         sidebar.appendChild(newCategory);
-    })
+        //adds event listeners for content
+        newCategory.addEventListener('click',()=>{
+            loadContent(category);
+        })
+        })
 }
 
 export {loadSidebar};
