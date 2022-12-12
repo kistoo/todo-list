aeaa26f36deb13bd31836b7093f860a2d7d01b66
kistoo <tomio.nakamura15@gmail.com>
2022-12-12 10:33:52 -0500

    Adjust function with dataStorage values

--- src/loadSidebar.js
+++ src/loadSidebar.js
@@ -1,12 +1,14 @@
+import { dataStorage } from "./dataStorage";
+
 function loadSidebar() {
     const sidebar = document.getElementById('sidebar');
-    const categories = ["Today","Odin projects","Social","Debts"];
 
+    const categories = dataStorage.categories.getCategory();
     //this function need to be in dataStorage module
     categories.forEach((category)=> {
         const newCategory = document.createElement('div');
         newCategory.className = "category";
-        newCategory.textContent = category;
+        newCategory.textContent = category.name;
         sidebar.appendChild(newCategory);
     })
 }
