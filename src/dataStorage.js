f4a2b6ca8743a46fd1c4f44ab227e725c5b9f93e
kistoo <tomio.nakamura15@gmail.com>
2022-12-11 23:55:09 -0500

    Add categories storage

---
+++ src/dataStorage.js
@@ -0,0 +1,81 @@
+const dataStorage = (() => {
+
+    //admins categories
+    const categories = (() => {
+
+        const categoriesFactory = (name) => {
+            let todos = [];
+            return {
+                name,
+                todos
+            }
+        }
+
+        let names = [categoriesFactory("Today"),categoriesFactory("Odin projects"),categoriesFactory("Social"),categoriesFactory("Debts")];
+        
+        const getCategory = function() {
+            return names;
+        }
+
+        const addCategory = function(name) {
+            names.push(name);
+        }
+
+        const removeCategory = function(category) {
+            names.splice(getIndex(category,names),1);
+            addTodo("Today","cook","nothing","tomorrow","!!!",false)
+            removeTodo("Today","cook");
+            console.log(names)
+        }
+
+        //gets index of object.name in names
+        const getIndex = function(name,array) {
+            let index;
+            for (let i = 0 ; i < array.length; i++) {
+                if (array[i].name === name) {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        //todos
+
+        const todosFactory = (category,name,description,dueDate,priority,isDone) => {
+            return {
+                category,
+                name,
+                description,
+                dueDate,
+                priority,
+                isDone
+            };
+        }
+
+        const addTodo = function(category,name,description,dueDate,priority,isDone) {
+            const newTodo = todosFactory(category,name,description,dueDate,priority,isDone);
+            names[getIndex(category,names)].todos.push(newTodo);
+        }
+
+        const removeTodo = function(category, name) {
+            const todoCategory = names[getIndex(category,names)];
+            const todos = todoCategory.todos;
+            const todo = todos[getIndex(name,todos)];
+            todos.splice(getIndex(name,todos))
+        }
+
+        return {
+            addCategory,
+            getCategory,
+            removeCategory
+        }
+
+    })();
+    
+    return {
+        categories
+    }
+
+})();
+
+export {dataStorage}
