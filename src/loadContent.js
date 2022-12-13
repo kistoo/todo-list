6f4c8ea84c6eea3569ea1f972275b275049a5d84
kistoo <tomio.nakamura15@gmail.com>
2022-12-12 20:20:00 -0500

    Load and sorts todos

--- src/loadContent.js
+++ src/loadContent.js
@@ -1,76 +1,131 @@
 import { dataStorage } from "./dataStorage";
+import edit from './images/pencil.svg';
+import trash from './images/delete.svg';
+import plus from './images/plus.svg';
 
 function loadContent(category = dataStorage.categories.getCategory("Today")) {
     //empties actual content
     const content = document.getElementById('content');
-    content.innerHTML="";
+    content.innerHTML = "";
+    const list = document.createElement('ul');
+    list.className = "todos";
 
+    const importance5 = document.createElement('div');
+    const importance4 = document.createElement('div');
+    const importance3 = document.createElement('div');
+    const importance2 = document.createElement('div');
+    const importance1 = document.createElement('div');
+    const importance0 = document.createElement('div');
+    
+    let hasTodos = false;
+    //imports todos
+    category.todos.forEach((todo) => {
+        hasTodos = true;
+
+        const tododiv = document.createElement('div');
+        tododiv.className = "todo";
+
+        const left = document.createElement('div');
+        left.className = "left";
+        const importance = document.createElement('div');
+        importance.className = `importance ${todo.priority}`;
+        left.appendChild(importance);
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.id = "isdone";
+        if (todo.priority.includes("done") === true) {
+            checkbox.checked = true;
+        }
+        checkbox.addEventListener('click',() => {
+            if (todo.priority.includes("done") === false) {
+                todo.priority += " done";
+            } else {
+                todo.priority = todo.priority.replace(" done","");
+            }
+            loadContent(category);
+        });
+        left.appendChild(checkbox);
+        const name = document.createElement('h2');
+        name.textContent = todo.name;
+        left.appendChild(name);
+        tododiv.appendChild(left);
+
+        const right = document.createElement('div');
+        right.className = "right";
+        //replace later with actual date 
+        const date = document.createElement('h2');
+        date.textContent = todo.dueDate;
+        right.appendChild(date);
+        const editIcon = new Image();
+        editIcon.src = edit;
+        right.appendChild(editIcon);
+        const deleteIcon = new Image();
+        deleteIcon.src = trash;
+        right.appendChild(deleteIcon);
+        tododiv.appendChild(right);
+
+        //orders by priority
+
+        if (todo.priority.includes("done") === true) {
+            importance0.appendChild(tododiv);
+        } else {
+            switch (todo.priority) {
+                case "one":
+                    importance1.appendChild(tododiv);
+                    break;
+                case "two":
+                    importance2.appendChild(tododiv);
+                    break;
+                case "three":
+                    importance3.appendChild(tododiv);
+                    break;
+                case "four":
+                    importance4.appendChild(tododiv);
+                    break;
+                case "five":
+                    importance5.appendChild(tododiv);
+                    break;           
+            }
+        }
+    });
+
+    list.appendChild(importance5);
+    list.appendChild(importance4);
+    list.appendChild(importance3);
+    list.appendChild(importance2);
+    list.appendChild(importance1);
+    list.appendChild(importance0);
+
+    //category = today
     if (category.name === "Today") {
         const greeting = document.createElement('h1');
         greeting.textContent = "Good Morning!";
         content.appendChild(greeting);
 
         const text = document.createElement('h3');
-        text.textContent = "Today, you have this thing to do:";
-        content.appendChild(text);
+        //verifies if theres a todo
+        if (hasTodos === false) {
+            text.textContent = "Today you are free! There is nothing to do :D";
+            content.appendChild(text);
+        } else {
+            text.textContent = "Today, you have this thing to do:";
+            content.appendChild(text);
+            content.appendChild(list);
+        }
 
-        const list = document.createElement('ul');
-        //sort todos by importance
-        const importance5 = document.createElement('ul');
-        importance5.textContent = "!!!!!";
-        importance5.style.color="#b91c1c";
-        const importance4 = document.createElement('ul');
-        importance4.textContent = "!!!!";
-        importance4.style.color="#c2410c";
-        const importance3 = document.createElement('ul');
-        importance3.textContent = "!!!";
-        importance3.style.color="#a16207";
-        const importance2 = document.createElement('ul');
-        importance2.textContent = "!!";
-        importance2.style.color="#15803d";
-        const importance1 = document.createElement('ul');
-        importance1.textContent = "!";
-        importance1.style.color="#0e7490";
-        category.todos.forEach((todo) => {
-            const thingToDo = document.createElement('li');
-            thingToDo.textContent = todo.name;
-            switch (todo.priority) {
-                case "!!!!!":
-                    importance5.appendChild(thingToDo);
-                    break;
-                case "!!!!":
-                    importance4.appendChild(thingToDo);
-                    break;
-                case "!!!":
-                    importance3.appendChild(thingToDo);
-                    break;
-                case "!!":
-                    importance2.appendChild(thingToDo);
-                    break;
-                case "!":
-                    importance1.appendChild(thingToDo);
-                    break;
-            }
-        })
-        if (importance5.children.length > 0) {
-            list.appendChild(importance5);
+    } else { //category = another category
+        const categoryName = document.createElement('h1');
+        categoryName.textContent = category.name;
+        content.appendChild(categoryName);
+
+        if (hasTodos === false) {
+            const text = document.createElement('h3');
+            text.textContent = 'There is nothing to do here, add activities with the "+" button';
+            content.appendChild(text);
+        } else {
+            content.appendChild(list);
         }
-        if (importance4.children.length > 0) {
-            list.appendChild(importance4);
-        }
-        if (importance3.children.length > 0) {
-            list.appendChild(importance3);
-        }
-        if (importance2.children.length > 0) {
-            list.appendChild(importance2);
-        }
-        if (importance1.children.length > 0) {
-            list.appendChild(importance1);
-        }
-        content.appendChild(list);
     }
-
-
 
 }
 
