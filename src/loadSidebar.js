0d313cf55dfb1fa5fd91da7025cacaaafdb11523
kistoo <tomio.nakamura15@gmail.com>
2022-12-12 21:58:55 -0500

    Add add category + style

--- src/loadSidebar.js
+++ src/loadSidebar.js
@@ -1,5 +1,6 @@
 import { dataStorage } from "./dataStorage";
 import { loadContent } from "./loadContent";
+import plus from "./images/plus.svg";
 
 function loadSidebar() {
     const sidebar = document.getElementById('sidebar');
@@ -15,7 +16,14 @@ function loadSidebar() {
         newCategory.addEventListener('click',()=>{
             loadContent(category);
         })
-        })
+    })
+    const addCategory = document.createElement('div');
+    addCategory.className = "add";
+    addCategory.textContent = "Add category";
+    const add = new Image();
+    add.src = plus;
+    addCategory.appendChild(add);
+    sidebar.appendChild(addCategory);
 }
 
 export {loadSidebar};
