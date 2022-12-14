42f817642db2e939e0c4b02008931c32456aaec0
kistoo <tomio.nakamura15@gmail.com>
2022-12-14 18:09:06 -0500

    Remove prebuilt todos

--- src/index.js
+++ src/index.js
@@ -6,12 +6,4 @@ import { loadContent } from './loadContent';
 
 loadHeader();
 loadSidebar();
-dataStorage.categories.addTodo('Odin projects','cook','2022-12-15','one')
-dataStorage.categories.addTodo('Debts','cook','2022-12-15','four')
-dataStorage.categories.addTodo('Today','cook','2022-12-15','two')
-dataStorage.categories.addTodo('Today','cook','2022-12-15','one')
-dataStorage.categories.addTodo('Today','cook','2022-12-15','five')
-dataStorage.categories.addTodo('Today','cook','2022-12-15','three')
-dataStorage.categories.addTodo('Today','cook','2022-12-15','four')
-
 loadContent(dataStorage.categories.getCategory());
