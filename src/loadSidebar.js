edd609c50e40112bd4b5b3507a70e0009dd2df5b
kistoo <tomio.nakamura15@gmail.com>
2022-12-13 21:21:42 -0500

    Add add category + style

--- src/loadSidebar.js
+++ src/loadSidebar.js
@@ -4,6 +4,7 @@ import plus from "./images/plus.svg";
 
 function loadSidebar() {
     const sidebar = document.getElementById('sidebar');
+    sidebar.innerHTML = "";
 
     //gets array with categories
     const categories = dataStorage.categories.getCategories();
@@ -24,6 +25,25 @@ function loadSidebar() {
     add.src = plus;
     addCategory.appendChild(add);
     sidebar.appendChild(addCategory);
+    //event listner to create new category
+    addCategory.addEventListener('click', () => {
+       sidebar.removeChild(addCategory)
+       const input = document.createElement('input');
+       input.type = "text";
+       sidebar.appendChild(input); 
+       input.focus();
+       input.addEventListener('keydown', (key) => {
+        if (key.key === "Enter" && input.value !== "") {
+            dataStorage.categories.addCategory(input.value);
+            loadSidebar();
+        } else if (key.key === "Escape") {
+            loadSidebar();
+        }
+       });
+       input.addEventListener('blur', () => {
+            loadSidebar();
+       })
+    })
 }
 
 export {loadSidebar};
